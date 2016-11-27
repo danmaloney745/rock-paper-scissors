92465a161d5f6937cdc881844ca13bf31691e77a
Tech-A39 <Tech-A39@tech-a39s-air.home>
2016-11-27 03:56:21 +0000

    Finished basic console version of the game

--- js/app.js
+++ js/app.js
@@ -9,12 +9,38 @@ let playerChoice = function() {
 }
 
 let computerChoice = function() {
-    console.log("Computer is thinking...");
     let compInput = Math.round(Math.random()* 3) + 1;
     compInput = gameArray[compInput];
     console.log(compInput);
+    return compInput;
 }
 
 
-computerChoice();
-playerChoice();
+function gameManager(computerChoice, playerChoice) {
+    computerChoice = computerChoice();
+    playerChoice = playerChoice();
+
+    if(computerChoice === playerChoice) {
+        console.log("The game was a draw");
+    }
+    else if(computerChoice === 'Rock' && playerChoice === 'Scissors') {
+        console.log('Computer Wins');
+    }
+    else if(playerChoice === 'Rock' && computerChoice === 'Scissors') {
+        console.log('Player Wins');
+    }
+    else if(computerChoice === 'Paper' && playerChoice === 'Rock') {
+        console.log('Computer Wins');
+    }
+    else if(playerChoice === 'Paper' && computerChoice === 'Rock') {
+        console.log('Player WIns');
+    }
+    else if(computerChoice === "Scissors" && computerChoice === "Paper"){
+        console.log("Computer Wins");
+    }
+    else if(playerChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("Player Wins");
+    }
+}
+
+gameManager(computerChoice, playerChoice);

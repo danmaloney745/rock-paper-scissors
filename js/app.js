19953fa1e0ac1ea1e394c07ce2306124b1a8b997
Tech-A39 <Tech-A39@tech-a39s-air.home>
2016-11-27 03:03:42 +0000

    Refactored computer input method

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let gameArray = ['rock', 'paper', 'scissors'];
+let gameArray = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let compScore = 0;
 
@@ -11,19 +11,7 @@ let playerChoice = function() {
 let computerChoice = function() {
     console.log("Computer is thinking...");
     let compInput = Math.round(Math.random()* 3) + 1;
-    
-    switch(compInput) {
-        case 1:
-            compInput = "Rock";
-            break;
-        case 2:
-            compInput = "Scissors";
-            break;
-        case 3:
-            compInput = "Paper";
-            break;
-    }
-
+    compInput = gameArray[compInput];
     console.log(compInput);
 }
 

cee236811cb52c21c491eecc47468774f875049c
Tech-A39 <Tech-A39@rich_0121m.uk.brad-ex.co.uk>
2016-11-28 15:51:27 +0000

    UI now initiates the game and displays the score and results

--- js/app.js
+++ js/app.js
@@ -1,55 +1,71 @@
-let gameArray = ['Rock', 'Paper', 'Scissors'];
-let playerScore = 0;
-let compScore = 0;
+$(function(){
+    let gameArray = ['Rock', 'Paper', 'Scissors'];
+    let playerScore = 0;
+    let compScore = 0;
+    let playerChoice;
 
-let playerChoice = function() {
-    let userInput = prompt("Enter either rock/paper/scissors");
-    console.log(userInput);
-    return userInput;   
-}
-
-let computerChoice = function() {
-    let compInput = Math.round(Math.random()* 2) + 0;
-    compInput = gameArray[compInput];
-    console.log(compInput);
-    return compInput;
-}
-
-
-function gameManager(computerChoice, playerChoice) {
-    computerChoice = computerChoice();
-    playerChoice = playerChoice();
-
-    if(computerChoice === playerChoice) {
-        console.log("The game was a draw");
-    }
-    else if(computerChoice === 'Rock' && playerChoice === 'Scissors') {
-        console.log('Computer Wins');
-        compScore++;
-    }
-    else if(playerChoice === 'Rock' && computerChoice === 'Scissors') {
-        console.log('Player Wins');
-        playerScore++;
-    }
-    else if(computerChoice === 'Paper' && playerChoice === 'Rock') {
-        console.log('Computer Wins');
-        compScore++;
-    }
-    else if(playerChoice === 'Paper' && computerChoice === 'Rock') {
-        console.log('Player WIns');
-        playerScore++;
-    }
-    else if(computerChoice === "Scissors" && computerChoice === "Paper"){
-        console.log("Computer Wins");
-        compScore++;
-    }
-    else if(playerChoice === "Scissors" && computerChoice === "Paper") {
-        console.log("Player Wins");
-        playerScore++;
+    var buttons = document.querySelectorAll('.myButton');
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        button.addEventListener('click' , function(event) {
+            var playerChooses = this.id.substring(0, this.id.length);
+            gameManager(playerChooses);
+        })
     }
 
-    console.log(compScore);
-    console.log(playerScore);
-}
+    // function playerChoice(){
+    //     var buttons = document.querySelectorAll('.myButton');
+    //     for (var i = 0; i < buttons.length; i++) {
+    //         var button = buttons[i];
+    //         button.addEventListener('click' , function(event) {
+    //             var playerChooses = this.id.substring(0, this.id.length);
+    //             gameManager(playerChooses);
+    //         })
+    //     }
 
-gameManager(computerChoice, playerChoice);
+    // };
+
+    //Function that randomises and returns the computers choice
+    function computerChoice() {
+        let compInput = Math.round(Math.random()* 2) + 0;
+        compInput = gameArray[compInput];
+        return compInput;
+    }
+
+    //Function that calls the two choice functions and compares the values and ouput who wins.
+    function gameManager(playerChoice) {
+        let computer = computerChoice();
+        let player = playerChoice;
+        
+        console.log(computer);
+        console.log(player);
+
+        if(computer === playerChoice) {
+            console.log("The game was a draw");
+        }
+        else if(computer === 'Rock' && player === 'Scissors') {
+            console.log('Computer Wins');
+            compScore++;
+        }
+        else if(player === 'Rock' && computer === 'Scissors') {
+            console.log('Player Wins');
+            playerScore++;
+        }
+        else if(computer === 'Paper' && player === 'Rock') {
+            console.log('Computer Wins');
+            compScore++;
+        }
+        else if(player === 'Paper' && computer === 'Rock') {
+            console.log('Player WIns');
+            playerScore++;
+        }
+        else if(computer === "Scissors" && player === "Paper"){
+            console.log("Computer Wins");
+            compScore++;
+        }
+        else if(player === "Scissors" && computer === "Paper") {
+            console.log("Player Wins");
+            playerScore++;
+        }
+    }
+});
